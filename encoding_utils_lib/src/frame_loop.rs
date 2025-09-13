0bca7a848335c686926a999217b1ac3ba773bf34
admonisher-of-thieves <198962141+admonisher-of-thieves@users.noreply.github.com>
2025-09-12 20:31:47 -0600

    fix crf when chapters are use

--- encoding_utils_lib/src/frame_loop.rs
+++ encoding_utils_lib/src/frame_loop.rs
@@ -181,7 +181,6 @@ pub fn run_frame_loop<'a>(
 
     let mut scene_list_frames = scene_list.clone();
     scene_list_frames.with_zone_overrides(&temp_av1an_params, &temp_encoder_params);
-    scene_list_frames.update_crf(f64::MAX);
 
     // Zoning Chapters
     if !crf_chapters.is_empty()
@@ -203,10 +202,11 @@ pub fn run_frame_loop<'a>(
         let mut zone_chapters = ZoneChapters::from_chapters(&video, chapters);
         zone_chapters.with_crfs(crf_chapters);
         println!("{}", zone_chapters);
+        scene_list_frames.update_crf(f64::MAX);
         scene_list_frames.apply_zone_chapters(&zone_chapters);
+        scene_list_frames.update_crf_if_unzoned(*first_crf);
         scene_list.sync_crf_by_index(&scene_list_frames);
     }
-    scene_list_frames.update_crf_if_unzoned(*first_crf);
 
     let n_frames = match n_frames {
         Some(n_frames) => n_frames,
